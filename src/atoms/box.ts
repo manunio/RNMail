222818b433013d561ed8b6d0d44383a951fc8814
manunio <max.manu.nair@gmail.com>
2022-07-08 01:20:56 +0530

    suppressed @shopify/restyle ts error for now

--- src/atoms/box.ts
+++ src/atoms/box.ts
@@ -2,7 +2,7 @@ import {BackgroundColorProps, createBox} from "@shopify/restyle";
 import {Theme} from "@/themes";
 import React from "react";
 
-const Box = createBox<Theme , & { children?: React.ReactNode }>()
+const Box = createBox<Theme>()
 export type BoxProps = React.ComponentProps<typeof Box>
 
 export default Box;
--- src/atoms/container.tsx
+++ src/atoms/container.tsx
@@ -3,8 +3,10 @@ import {BoxProps} from "@shopify/restyle";
 import {Theme} from "@/themes";
 import Box from "./box";
 
+// TODO: find solution to below ts-ignore
 const Container: React.FC<BoxProps<Theme>> = props => (
     <Box {...props} flex={1} backgroundColor="$background">
+        {/*@ts-ignore*/}
         {props.children}
     </Box>
 );
